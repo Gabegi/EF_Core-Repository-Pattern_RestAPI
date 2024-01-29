b151d1f48cd70834070d15c1f50629e57c54adf8
gabriel <gabriel.pirastru@devoteam.com>
2024-01-29 09:24:10 +0100

    added service mapping

---
+++ BrandApplication/BrandApplication.Business/Services/IServices/IServiceMappings/MappingInterfaces.cs
@@ -0,0 +1,11 @@
+﻿using BrandApplication.Business.DTOs;
+using BrandApplication.DataAccess.Models;
+
+
+namespace BrandApplication.Business.Services.IServices.IServiceMappings
+{
+
+    public interface IBrandMapping: IReadServiceAsync<Brand, BrandDto>
+    {
+    }
+}
---
+++ BrandApplication/BrandApplication.Business/Services/ServiceMappings/ServiceMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BrandApplication.Business.DTOs;
+using BrandApplication.Business.Services.IServices.IServiceMappings;
+using BrandApplication.DataAccess.Interfaces;
+using BrandApplication.DataAccess.Models;
+
+namespace BrandApplication.Business.Services.ServiceMappings
+{
+    public class BrandMapping : ReadServiceAsync<Brand, BrandDto>, IBrandMapping
+    {
+        public BrandMapping(IGenericRepository<Brand> genericRepository, IMapper mapper) : base(genericRepository, mapper)
+        {
+        }
+    }
+}
